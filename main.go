9ae4c4be870bba57b6bb64d2a531446a454ed766
Adirelle <adirelle@gmail.com>
2016-08-02 13:27:21 +0200

    First attempt at rewriting main.go.

--- main.go
+++ main.go
@@ -1,181 +1,143 @@
-
 package main
 
 import (
-    "flag"
-    "time"
-    "strings"
-    "bufio"
-    "os"
-    "os/signal"
-    "syscall"
-    "log"
-    "regexp"
-    "errors"
-    "net/url"
-    "bytes"
+	"bufio"
+	"errors"
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"regexp"
+	"strings"
+	"sync"
+	"syscall"
+	"time"
 
-    elastigo "github.com/mattbaird/elastigo/lib"
+	"./indexer"
 )
 
-type IndexerSpec struct {
-    IndexPrefix string
-    DocType string
-    MaxConns int
-    BatchSize int
-    FlushDelay time.Duration
-    RetryDelay time.Duration
-}
+type Indexer struct {
+	Hosts       string
+	Protocol    string
+	Port        int
+	Username    string
+	Password    string
+	IndexPrefix string
+	DocType     string
+	BatchSize   int
+	FlushDelay  time.Duration
 
-type Event struct {
-    Timestamp string
-    Record *bytes.Buffer
+	actions   chan indexer.Action
+	batcher   indexer.Batcher
+	waitGroup sync.WaitGroup
 }
 
 var eventRegexp = regexp.MustCompile("^(\\d{4}\\.\\d{2}\\.\\d{2}) (\\{.+\\})$")
 
 func main() {
 
-    log.SetOutput(os.Stderr)
-    log.SetFlags(log.Ldate | log.Ltime | log.Lmicroseconds)
+	log.SetOutput(os.Stderr)
+	log.SetFlags(log.Ldate | log.Ltime | log.Lmicroseconds)
 
-    conn := elastigo.NewConn()
-    spec := parseFlags(conn)
+	indexer := Indexer{}
+	indexer.readFlags()
+	indexer.setup()
+	indexer.installSignalHandler()
 
-    done := make(chan struct{})
-    events := make(chan Event)
+	go indexer.runReader()
+	go indexer.runBatcher()
 
-    installSignalHandlers(done)
-    readEvents(events, done)
-    runIndexer(conn, spec, events, done)
+	indexer.waitGroup.Wait()
 }
 
-func parseFlags(conn *elastigo.Conn) IndexerSpec {
+func (i *Indexer) readFlags() {
+	flag.StringVar(&i.Hosts, "hosts", "localhost", "Comma-separated list of hosts")
 
-    servers := hosts{ []string{ "localhost" } }
+	flag.StringVar(&i.Protocol, "protocol", "http", "Protocol : http | https")
+	flag.IntVar(&i.Port, "port", 9200, "ElasticSearch port")
+	flag.StringVar(&i.Username, "user", "", "Username for authentication")
+	flag.StringVar(&i.Password, "passwd", "", "Password for authentication")
 
-    flag.Var(&servers, "hosts", "Comma-separated list of hosts")
+	flag.StringVar(&i.IndexPrefix, "index", "logs", "Index prefix")
+	flag.StringVar(&i.DocType, "type", "log", "Document type")
+	flag.IntVar(&i.BatchSize, "batch-size", 500, "Maximum number of events in a batch")
+	flag.DurationVar(&i.FlushDelay, "flush-delay", 1*time.Second, "Maximum delay between flushs")
 
-    flag.StringVar(&conn.Protocol, "protocol", "http", "Protocol : http | https")
-    flag.StringVar(&conn.Port, "port", "9200", "ElasticSearch port")
-    flag.StringVar(&conn.Username, "user", "", "Username for authentication")
-    flag.StringVar(&conn.Password, "passwd", "", "Password for authentication")
-    flag.BoolVar(&conn.Gzip, "compression", false, "Enable compression ?")
-
-    spec := IndexerSpec{}
-
-    flag.StringVar(&spec.IndexPrefix, "index", "logs-", "Index prefix")
-    flag.StringVar(&spec.DocType, "type", "log", "Document type")
-    flag.IntVar(&spec.MaxConns, "concurrency", 1, "Number of concurrent connections")
-    flag.IntVar(&spec.BatchSize, "batch-size", 500, "Maximum number of events in a batch")
-    flag.DurationVar(&spec.FlushDelay, "flush-delay", 1 * time.Second, "Maximum delay between flushs")
-    flag.DurationVar(&spec.RetryDelay, "retry-delay", 10 * time.Second, "Delay between retries of failed requests")
-
-    flag.Parse()
-
-    conn.Hosts = servers.Names
-
-    return spec
+	flag.Parse()
 }
 
-func readEvents(events chan Event, done chan struct{}) {
-    go func() {
-        scanner := bufio.NewScanner(os.Stdin)
-        scanner.Buffer(make([]byte, 64000), 64000)
-
-        defer func() {
-            close(events)
-            if err := scanner.Err(); err != nil {
-                log.Printf("Error on input: %s", err)
-            }
-        }()
-
-        for scanner.Scan() {
-            line := scanner.Bytes()
-            if event, err := parseLine(line); err == nil {
-                select {
-                case <- done:
-                    return
-                case events <- *event:
-                    // Suivant
-                }
-            } else {
-                log.Printf("%s: %q", err, line)
-            }
-        }
-    }()
+func (i *Indexer) setup() {
+	hosts := strings.Split(i.Hosts, ",")
+	requester := indexer.NewRequester(http.Client{}, hosts, i.Port, i.Protocol, i.Username, i.Password)
+	i.batcher = indexer.NewBatcher(requester, i.FlushDelay, i.BatchSize, 1024*i.BatchSize)
+	i.actions = make(chan indexer.Action)
 }
 
-func parseLine(line []byte) (*Event, error) {
-
-    matches := eventRegexp.FindSubmatch(line)
-
-    if matches == nil {
-        return nil, errors.New("Invalid input")
-    }
-
-    record := bytes.NewBuffer(matches[2])
-
-    return &Event{string(matches[1]), record}, nil
+func (i *Indexer) installSignalHandler() {
+	sigChan := make(chan os.Signal)
+	go func() {
+		sig := <-sigChan
+		log.Printf("Received signal %s", sig)
+		close(i.actions)
+	}()
+	signal.Notify(sigChan, syscall.SIGINT, syscall.SIGTERM)
 }
 
-func runIndexer(conn *elastigo.Conn, spec IndexerSpec, events <-chan Event, done chan struct{}) {
+func (i *Indexer) runReader() {
 
-    indexer := conn.NewBulkIndexerErrors(spec.MaxConns, int(spec.RetryDelay.Seconds()))
-    indexer.BulkMaxDocs = spec.BatchSize
-    indexer.BufferDelayMax = spec.FlushDelay
+	i.waitGroup.Add(1)
 
-    go func() {
-        for err := range indexer.ErrorChannel {
-            _, ok := err.Err.(*url.Error)
-            if ok || err.Err.Error() == "Unauthorized" {
-                log.Fatalf("Rejected request, error: %s", err.Err)
-            }
-            log.Printf("Rejected request, error: %T %#v %s, request:\n%s", err.Err, err.Err, err.Err, err.Buf.Bytes())
-        }
-    }()
+	scanner := bufio.NewScanner(os.Stdin)
+	scanner.Buffer(make([]byte, 64000), 64000)
+	defer func() {
+		if err := scanner.Err(); err != nil {
+			log.Printf("Error on input: %s", err)
+		}
+		close(i.actions)
+		log.Println("Stopped reader")
+		i.waitGroup.Done()
+	}()
 
-    indexer.Start()
+	log.Println("Started reader")
 
-    var num uint64 = 0;
-
-    for event := range events {
-        index := spec.IndexPrefix + event.Timestamp
-        num = num + 1;
-        if err := indexer.Index(index, spec.DocType, "", "", "", nil, event.Record); err != nil {
-            log.Printf("Cannot send record, %s: %q", err, event.Record)
-        }
-    }
-
-    log.Printf("Stopping indexation, %d pending record(s)", indexer.PendingDocuments())
-    indexer.Stop()
-    log.Printf("%d record(s) sent, %d error(s)", num, indexer.NumErrors())
+	for scanner.Scan() {
+		line := scanner.Bytes()
+		if action, err := i.parseLine(line); err == nil {
+			i.actions <- action
+		} else {
+			log.Printf("%s: %q", err, line)
+		}
+	}
 }
 
-func installSignalHandlers(done chan struct{}) {
-    sigChan := make(chan os.Signal)
-    go func() {
-        select {
-        case <- done:
-            // Noop
-        case sig := <-sigChan:
-            log.Printf("Received signal %s", sig)
-            close(done)
-        }
-    }()
+func (i *Indexer) runBatcher() {
+	i.waitGroup.Add(1)
+	defer func() {
+		i.batcher.Stop()
+		log.Println("Batcher stopped")
+		log.Printf("Batcher: received %d, sent %d, errors %d", i.batcher.Received(), i.batcher.Sent(), i.batcher.Errors())
+		i.waitGroup.Done()
+	}()
 
-    signal.Notify(sigChan, syscall.SIGINT, syscall.SIGTERM)
+	log.Println("Started batcher")
+	for a := range i.actions {
+		i.batcher.Send(a)
+	}
 }
 
-type hosts struct {
-    Names []string
-}
+func (i *Indexer) parseLine(line []byte) (indexer.Action, error) {
+	matches := eventRegexp.FindSubmatch(line)
 
-func (h *hosts) Set(v string) error {
-    h.Names = strings.Split(v, ",")
-    return nil
-}
+	if matches == nil {
+		return nil, errors.New("Invalid input")
+	}
 
-func (h *hosts) String() string {
-    return strings.Join(h.Names, ",")
+	return indexer.SimpleAction{
+		ID:       "",
+		Index:    fmt.Sprintf("%s-%s", i.IndexPrefix, matches[1]),
+		DocType:  i.DocType,
+		Document: matches[2],
+	}, nil
 }
