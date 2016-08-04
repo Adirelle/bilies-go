b0a63fc871ebd451ab32851d17c306d1381e1829
Adirelle <adirelle@gmail.com>
2016-08-04 11:36:51 +0200

    Fixed license header.

--- indexer/pool.go
+++ indexer/pool.go
@@ -1,6 +1,5 @@
 /*
 bilies-go - Bulk Insert Logs Into ElasticSearch
-<one line to give the program's name and a brief idea of what it does.>
 Copyright (C) 2016 Adirelle <adirelle@gmail.com>
 
 This program is free software: you can redistribute it and/or modify
