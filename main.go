6f0816f751ecc3c26d072c02512dd55728d56408
Adirelle <adirelle@gmail.com>
2016-08-08 13:40:33 +0200

    Added option to setup log and PID files.

--- main.go
+++ main.go
@@ -45,6 +45,9 @@ type config struct {
 	password    string
 	queueDir    string
 	logLevel    logging.Level
+	debug       bool
+	logFile     string
+	pidFile     string
 }
 
 func main() {
@@ -52,9 +55,13 @@ func main() {
 
 	parseFlags(&cfg)
 
-	logging.SetBackend(logging.NewLogBackend(os.Stderr, "", 0))
-	logging.SetLevel(cfg.logLevel, log.Module)
-	logging.SetFormatter(logging.MustStringFormatter("%{time} %{level}: %{message} (%{shortfile})"))
+	if cfg.pidFile != "" {
+		setupPidFile(cfg.pidFile)
+		defer os.Remove(cfg.pidFile)
+	}
+
+	logFile, logBackend := setupLogging(cfg.logFile, cfg.logLevel, cfg.debug)
+	defer logFile.Close()
 
 	queue, err := goque.OpenQueue(cfg.queueDir)
 	if err != nil {
@@ -85,7 +92,6 @@ func main() {
 
 func parseFlags(c *config) {
 	var (
-		debug   = false
 		verbose = false
 	)
 
@@ -104,16 +110,50 @@ func parseFlags(c *config) {
 	flag.IntVarP(&c.batchSize, "batch-size", "n", 500, "Maximum number of events in a batch")
 	flag.DurationVarP(&c.flushDelay, "flush-delay", "f", 1*time.Second, "Maximum delay between flushs")
 
-	flag.BoolVarP(&debug, "debug", "d", false, "Enable debug output")
+	flag.BoolVarP(&c.debug, "debug", "d", false, "Enable debug output")
 	flag.BoolVarP(&verbose, "verbose", "v", false, "Enable verbose")
+
+	flag.StringVar(&c.logFile, "log-file", "", "Write the logs into the file")
+	flag.StringVar(&c.pidFile, "pid-file", "", "Write the PID into that file")
 
 	flag.StringVarP(&c.queueDir, "quque", "q", defaultQueueDir, "Queue directory")
 
 	flag.Parse()
 
-	if debug {
+	if c.debug {
 		c.logLevel = logging.DEBUG
 	} else if verbose {
 		c.logLevel = logging.INFO
 	}
+}
+
+func setupPidFile(path string) {
+	if pidFile, err := os.Create(path); err == nil {
+		defer pidFile.Close()
+		if _, err = fmt.Fprintf(pidFile, "%d", os.Getpid()); err != nil {
+			log.Panicf("Could not write PID in %q: %s", path, err)
+		}
+	} else {
+		log.Panicf("Could not open PID file %q: %s", path, err)
+	}
+}
+
+func setupLogging(path string, level logging.Level, debug bool) (logFile *os.File, logBackend *logging.LogBackend) {
+	var err error
+	if path != "" {
+		if logFile, err = os.OpenFile(path, os.O_CREATE|os.O_APPEND|os.O_WRONLY|os.O_SYNC, 0640); err != nil {
+			log.Panicf("Cannot open logfile %q: %s", path, err)
+		}
+	} else {
+		logFile = os.Stderr
+	}
+	logBackend = logging.NewLogBackend(logFile, "", 0)
+	logging.SetBackend(logBackend)
+	logging.SetLevel(level, log.Module)
+	logFormat := "%{time} %{level}: %{message}"
+	if debug {
+		logFormat = "%{time} %{level}: %{message} (%{shortfile})"
+	}
+	logging.SetFormatter(logging.MustStringFormatter(logFormat))
+	return
 }
