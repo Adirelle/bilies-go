fc109dfd50ef89889502bff671f6f6e158a9e9f0
Adirelle <adirelle@gmail.com>
2016-08-12 16:02:51 +0200

    Rewrote the queue manager by using a goroutine per task (read, write, drop).

--- queue.go
+++ queue.go
@@ -19,19 +19,21 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 package main
 
 import (
+	"bytes"
 	"encoding/binary"
 	"encoding/json"
 	"fmt"
+	"io"
 	"sync"
 	"time"
 
 	metrics "github.com/rcrowley/go-metrics"
 	"github.com/syndtr/goleveldb/leveldb"
+	"github.com/syndtr/goleveldb/leveldb/iterator"
+	"github.com/syndtr/goleveldb/leveldb/util"
 )
 
 var (
-	queueBytesPool = sync.Pool{New: makeBytes}
-
 	converter = binary.BigEndian
 
 	mQueue               = metrics.NewPrefixedChildRegistry(mRoot, "queue.")
@@ -39,24 +41,16 @@ var (
 	mQueueWrittenRecords = metrics.GetOrRegisterMeter("write.records", mQueue)
 	mQueueReadBytes      = metrics.GetOrRegisterMeter("read.bytes", mQueue)
 	mQueueReadRecords    = metrics.GetOrRegisterMeter("read.records", mQueue)
-	mQueueLen            = metrics.GetOrRegisterHistogram("size", mQueue, metrics.NewUniformSample(1e5))
-	mQueuePending        = metrics.GetOrRegisterHistogram("pending", mQueue, metrics.NewUniformSample(1e5))
 )
 
 type Queue struct {
-	db *leveldb.DB
-
 	WriteC chan<- InputRecord
 	ReadC  <-chan InputRecord
 	DropC  chan<- int
 
-	sync chan bool
-}
-
-// InputRecord defines the expected schema of input.
-type InputRecord struct {
-	Suffix   string          `json:"date"`
-	Document json.RawMessage `json:"log"`
+	db    *leveldb.DB
+	close chan bool
+	ended sync.WaitGroup
 }
 
 func OpenQueue(path string) (*Queue, error) {
@@ -65,145 +59,214 @@ func OpenQueue(path string) (*Queue, error) {
 		return nil, err
 	}
 
-	q := &Queue{db: db, sync: make(chan bool)}
-	go q.process()
-	<-q.sync
+	writeChan := make(chan InputRecord)
+	readChan := make(chan InputRecord)
+	dropChan := make(chan int)
+
+	q := &Queue{
+		db:     db,
+		WriteC: writeChan,
+		ReadC:  readChan,
+		DropC:  dropChan,
+		close:  make(chan bool),
+	}
+
+	started := &sync.WaitGroup{}
+	started.Add(3)
+
+	log.Debug("Starting queue")
+
+	go q.processReads(readChan, started)
+	go q.processWrites(writeChan, started)
+	go q.processDrops(dropChan, started)
+
+	started.Wait()
+	log.Debug("Queue started")
+
 	return q, nil
 }
 
 func (q *Queue) Close() {
-	q.sync <- false
-	<-q.sync
+	close(q.close)
+	q.ended.Wait()
+	q.db.Close()
 }
 
-func (q *Queue) process() {
+func (q *Queue) processWrites(input chan InputRecord, started *sync.WaitGroup) {
 	var (
-		writeChan = make(chan InputRecord)
-		readChan  = make(chan InputRecord)
-		dropChan  = make(chan int)
+		iter = q.db.NewIterator(nil, nil)
+		buf  = bytes.Buffer{}
 
-		ticker = time.NewTicker(1 * time.Second)
-
-		outRec  InputRecord
-		outChan chan InputRecord
-
-		writeID, readID, dropID DbKey
+		lastID DbKey
 	)
 
-	defer q.db.Close()
-
-	iter := q.db.NewIterator(nil, nil)
 	if iter.First() {
-		readID = FromBytes(iter.Key()) - 1
-	}
-	dropID = readID
-	if iter.Last() {
-		writeID = FromBytes(iter.Key())
+		lastID = FromBytes(iter.Key())
 	}
 	iter.Release()
 
-	q.ReadC = readChan
-	q.WriteC = writeChan
-	q.DropC = dropChan
-
-	defer ticker.Stop()
-	defer close(readChan)
-	defer close(q.sync)
-
-	q.sync <- true
+	q.ended.Add(1)
+	defer q.ended.Done()
+	started.Done()
 
 	for {
 		select {
-		case inRec := <-writeChan:
-			writeID++
-			q.write(writeID, inRec)
-		case outChan <- outRec:
-			outChan = nil
-		case num := <-dropChan:
-			next := dropID + DbKey(num)
-			if next > readID {
-				next = readID
+		case rec := <-input:
+			lastID++
+			rec.Marshall(&buf)
+			if err := q.db.Put(lastID.Bytes(), buf.Bytes(), nil); err == nil {
+				mQueueWrittenBytes.Mark(int64(buf.Len()))
+				mQueueWrittenRecords.Mark(1)
 			}
-			if err := q.drop(dropID, next); err == nil {
-				dropID = next
-				log.Debugf("Unqueueq %d records", num)
-			} else {
-				log.Errorf("Could not unqueue %d records: %s", num, err)
-			}
-		case <-q.sync:
-			log.Debug("Bailing out")
+			buf.Reset()
+		case <-q.close:
 			return
-		case <-ticker.C:
-			mQueueLen.Update(int64(writeID - dropID))
-			mQueuePending.Update(int64(readID - dropID))
 		}
-		if outChan == nil && readID < writeID {
-			readID++
-			if err := q.read(readID, &outRec); err == nil {
-				outChan = readChan
+	}
+}
+
+func (q *Queue) processReads(output chan InputRecord, started *sync.WaitGroup) {
+	var (
+		iter   iterator.Iterator
+		lastID DbKey
+		rec    InputRecord
+		ch     chan InputRecord
+		delay  <-chan time.Time
+	)
+
+	q.ended.Add(1)
+	defer q.ended.Done()
+	started.Done()
+
+	for {
+		if ch == nil {
+			if iter == nil {
+				iter = q.db.NewIterator(&util.Range{Start: lastID.Bytes()}, nil)
+				iter.First()
+			}
+			if iter.Next() {
+				lastID = FromBytes(iter.Key())
+				rec.Unmarshall(iter.Value())
+				mQueueReadBytes.Mark(int64(len(iter.Value())))
+				mQueueReadRecords.Mark(1)
+				ch = output
 			} else {
-				log.Errorf("Cannot read from queue: %s", err)
+				iter.Release()
+				iter = nil
+				delay = time.After(flushDelay)
 			}
 		}
+		select {
+		case ch <- rec:
+			ch = nil
+		case <-delay:
+			delay = nil
+		case <-q.close:
+			return
+		}
 	}
 }
 
-func (q *Queue) write(id DbKey, rec InputRecord) (err error) {
-	buf := queueBytesPool.Get().([]byte)[:4]
-	defer queueBytesPool.Put(buf)
+func (q *Queue) processDrops(input chan int, started *sync.WaitGroup) {
+	q.ended.Add(1)
+	defer q.ended.Done()
+	started.Done()
 
-	suffix := []byte(rec.Suffix)
-	doc := []byte(rec.Document)
-	converter.PutUint32(buf[0:4], uint32(len(suffix)))
-	buf = append(buf, suffix...)
-	buf = append(buf, doc...)
-
-	if err = q.db.Put(id.Bytes(), buf, nil); err == nil {
-		mQueueWrittenBytes.Mark(int64(len(buf)))
-		mQueueWrittenRecords.Mark(1)
+	for {
+		select {
+		case n := <-input:
+			iter := q.db.NewIterator(nil, nil)
+			b := leveldb.Batch{}
+			var i int
+			for found := iter.First(); i < n && found; found = iter.Next() {
+				b.Delete(iter.Key())
+				i++
+			}
+			if err := q.db.Write(&b, nil); err == nil {
+				log.Debugf("Removed %d/%d records", i, n)
+			} else {
+				log.Debugf("Error removing record %d records: %s", n, err)
+			}
+			iter.Release()
+		case <-q.close:
+			return
+		}
 	}
-
-	return
-}
-
-func (q *Queue) read(id DbKey, rec *InputRecord) (err error) {
-	buf, err := q.db.Get(id.Bytes(), nil)
-	if err != nil {
-		return err
-	}
-	mQueueReadBytes.Mark(int64(len(buf)))
-	mQueueReadRecords.Mark(1)
-
-	len := converter.Uint32(buf[0:4])
-	rec.Suffix = string(buf[4 : 4+len])
-	rec.Document = json.RawMessage(buf[4+len:])
-	return nil
-}
-
-func (q *Queue) drop(from DbKey, to DbKey) (err error) {
-	b := &leveldb.Batch{}
-	for id := from; id < to; id++ {
-		b.Delete(id.Bytes())
-	}
-	return q.db.Write(b, nil)
-}
-
-func makeBytes() interface{} {
-	return make([]byte, 0, 1024)
 }
 
 type DbKey uint64
 
 func FromBytes(b []byte) DbKey {
+	if len(b) != 8 || b == nil {
+		return DbKey(0)
+	}
 	return DbKey(converter.Uint64(b))
 }
 
 func (k DbKey) Bytes() []byte {
+	if k == 0 {
+		return nil
+	}
 	b := make([]byte, 8)
 	converter.PutUint64(b, uint64(k))
 	return b
 }
 
 func (k DbKey) String() string {
+	if k == 0 {
+		return "<nil>"
+	}
 	return fmt.Sprintf("#%08x", uint64(k))
+}
+
+// InputRecord defines the expected schema of input.
+type InputRecord struct {
+	Suffix   string          `json:"date"`
+	Document json.RawMessage `json:"log"`
+}
+
+func (i InputRecord) String() string {
+	return fmt.Sprintf("suffix=%s doc=%q", i.Suffix, i.Document)
+}
+
+func (i InputRecord) Marshall(w io.Writer) {
+	var buf [4]byte
+	converter.PutUint32(buf[:], uint32(len(i.Suffix)))
+	w.Write(buf[:])
+	w.Write([]byte(i.Suffix))
+	w.Write(i.Document)
+}
+
+func (i *InputRecord) Unmarshall(buf []byte) {
+	len := converter.Uint32(buf[0:4])
+	i.Suffix = string(buf[4 : 4+len])
+	i.Document = json.RawMessage(buf[4+len:])
+}
+
+type IteratorWrapper struct {
+	underlying iterator.Iterator
+	hadFirst   bool
+}
+
+func WrapIterator(i iterator.Iterator) IteratorWrapper {
+	return IteratorWrapper{underlying: i}
+}
+
+func (i *IteratorWrapper) Next() (key []byte, value []byte, ok bool) {
+	if i.hadFirst {
+		ok = i.underlying.Next()
+	} else {
+		ok = i.underlying.First()
+		i.hadFirst = ok
+	}
+	if ok {
+		key = i.underlying.Key()
+		value = i.underlying.Value()
+	}
+	log.Debugf("Has record: %t, key=%s, len=%d bytes", ok, FromBytes(key), len(value))
+	return
+}
+
+func (i *IteratorWrapper) Release() {
+	i.underlying.Release()
 }
